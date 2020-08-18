49dd37433a067d5483027875d5ecd2dfdd150c1b
Nick Oosthuizen <nickoosthuizen@g.ucla.edu>
2020-08-18 15:04:37 -0500

    Header tree is now encoded and decoded on a bitwise level similar to the file data

--- decoder.py
+++ decoder.py
@@ -4,7 +4,6 @@ from node import Node
 class Decoder():
     def __init__(self, encoded_file):
         self.encoded_file = encoded_file
-        self.tree_byte_length = 0
         self.uncompressed_length = 0
         self.tree_info = ""
         self.compressed_data = ""
@@ -14,36 +13,57 @@ class Decoder():
         f = open(self.encoded_file, "rb")
         bytes_in_file = f.read()
         try:
-            self.tree_byte_length = int.from_bytes(bytes_in_file[0:4], "big")
+            tree_byte_length = int.from_bytes(bytes_in_file[0:4], "big")
             self.uncompressed_length = int.from_bytes(bytes_in_file[4:8], "big")
         except ValueError:
             print("Compressed file is incorrectly formatted")
             return
         try:
-            self.tree_info = bytes_in_file[8:(8 + self.tree_byte_length)].decode()
+            tree_bytes = bytes_in_file[8:(8 + tree_byte_length)]
         except ValueError:
             print("Error trying to decode tree")
             return 
-        for compressed_byte in bytes_in_file[(8 + self.tree_byte_length):]:
+        for compressed_byte in tree_bytes:
+            self.tree_info += format(compressed_byte, "08b")
+        for compressed_byte in bytes_in_file[(8 + tree_byte_length):]:
             self.compressed_data += format(compressed_byte, "08b")
         f.close()
+
+    def decode_tree_string(self):
+        if self.tree_info == "":
+            print("No tree bit string to write tree info")
+            return
+        converted_tree = []
+        i = 0
+        while i < len(self.tree_info):
+            if self.tree_info[i] == "0":
+                converted_tree.append("0")
+                i += 1
+            elif self.tree_info[i] == "1":
+                converted_tree.append("1")
+                i += 1
+                new_byte = self.tree_info[i:i + 8]
+                converted_tree.append(int(new_byte, 2).to_bytes(1, "big"))
+                i += 8
+        self.tree_info = converted_tree
 
     def construct_tree(self):
         if self.tree_info == "":
             print("No tree data to construct tree")
             return
         node_stack = []
-        for i in range(self.tree_byte_length):
+        for i in range(len(self.tree_info)):
             if self.tree_info[i] == "1":
                 node_stack.append(Node(value = self.tree_info[i+1]))
             elif self.tree_info[i] == "0":
                 if len(node_stack) == 1:
-                    self.root = node_stack.pop()
                     break
                 parent_node = Node()
                 parent_node.right_child = node_stack.pop()
                 parent_node.left_child = node_stack.pop()
                 node_stack.append(parent_node)
+        self.root = node_stack.pop()
+        
 
     def decode_data(self):
         if (self.compressed_data == ""):
@@ -52,28 +72,36 @@ class Decoder():
         if self.root is None:
             print("No tree with which to decode data")
             return
-        decoded_string = ""
+        decoded_bytes = []
         searcher = self.root
         counter = 0
         for step in self.compressed_data:
             if counter >= self.uncompressed_length:
                 break
-            if searcher is None:
-                break
-            if searcher.value is not None:
-                decoded_string += searcher.value
-                searcher = self.root
-                counter += 1
             if step == "0":
                 searcher = searcher.left_child
             else:
                 searcher = searcher.right_child
-        return decoded_string
+            if searcher is None:
+                break
+            if searcher.value is not None:
+                decoded_bytes.append(searcher.value)
+                searcher = self.root
+                counter += 1
+        return decoded_bytes
+    
+    def write_data(self, write_data):
+        wf = open("output.txt", "xb")
+        for write_byte in write_data:
+            wf.write(write_byte)
+        wf.close()
+
     
 
 
 check = Decoder("output.data")
 check.read_in_file_data()
+check.decode_tree_string()
 check.construct_tree()
 decoded = check.decode_data()
-print(decoded)
+check.write_data(decoded)
--- encoder.py
+++ encoder.py
@@ -7,11 +7,13 @@ from node import Node
 class Encoder():
     def __init__(self):
         self.encodings = {}
-        self.tree_string = ""
+        self.tree = []
 
+    # the frequencies of each byte in file is needed
+    # in order to construct a huffman encoding
     def get_frequencies(self, file):
         frequencies = {}
-        f = open(file)
+        f = open(file, "rb")
         bytes_in_file = f.read()
         for byte_to_process in bytes_in_file:
             if byte_to_process in frequencies:
@@ -21,14 +23,20 @@ class Encoder():
         f.close()
         return frequencies
     
+
     def generate_tree(self, frequencies):
         frequency_heap = []
+        # take each byte from the file and construct it into a min heap 
+        # with the lowest frequency bytes at the top
         for char in frequencies:
             new_node = Node(char)
             priority_grouping = [frequencies[char], 0, char, new_node]
             frequency_heap.append(priority_grouping)
         heapq.heapify(frequency_heap)
         count = 0
+        # until there is only 1 node left, take the two lowest frequency bytes
+        # and join them to a parent node. This parent node has the combined
+        # frequencies of its children and is added to the heap
         while len(frequency_heap) > 1:
             parent_node = Node()
             left_child = heapq.heappop(frequency_heap)
@@ -41,32 +49,32 @@ class Encoder():
         root = heapq.heappop(frequency_heap)
         return root[3] 
     
-    def find_char_encoding(self, root, current_path):
+    # recursively traverse the tree to get the byte encodings (0 = left, 1 = right)
+    def find_byte_encoding(self, root, current_path):
         if root is None:
             return
         if root.value is not None:
             self.encodings[root.value] = current_path
-        self.find_char_encoding(root.left_child, current_path + "0")
-        self.find_char_encoding(root.right_child, current_path + "1")
+        self.find_byte_encoding(root.left_child, current_path + "0")
+        self.find_byte_encoding(root.right_child, current_path + "1")
 
     def write_compressed_file_data(self, file):
         if self.encodings == {}:
             print("No encodings to use for compression")
             return
-        if self.tree_string == "":
+        if self.tree == []:
             print("Tree has not been stored")
             return
-        f = open(file, "r")
+
+        f = open(file, "rb")
         bytes_in_file = f.read()
         data_bytes = []
         write_byte = ""
-        compressed_data = ""
-        wf = open('output.data', 'xb')
         
-    
         for byte_to_write in bytes_in_file:
-            byte_encoding = self.encodings[byte_to_write]
-            write_byte += byte_encoding
+            write_byte += self.encodings[byte_to_write]
+            # once a bit string is at least 8 characters long, convert
+            # it to a byte to be written
             while len(write_byte) >= 8:
                 new_byte = write_byte[0:8]
                 data_bytes.append((int(new_byte, 2).to_bytes(1, "big")))
@@ -74,39 +82,81 @@ class Encoder():
                     write_byte = write_byte[8:]
                 else:
                     write_byte = ""
+        # if the encoding still has empty space, fill with 0s
         if write_byte != "":
             while len(write_byte) < 8:
                 write_byte += "0"
             data_bytes.append((int(write_byte, 2).to_bytes(1, "big")))
         
-        self.tree_string += "0"
-        # wf.write(len(data_bytes).to_bytes(4, "big"))
-        wf.write(len(self.tree_string).to_bytes(4, "big"))
+        encoded_tree_bytes = self.header_tree_to_bytes(self.tree)
+
+        wf = open("output.data", "xb")
+        wf.write(len(encoded_tree_bytes).to_bytes(4, "big"))
         wf.write(len(bytes_in_file).to_bytes(4, "big"))
-        encoded_string = self.tree_string.encode()
-        wf.write(encoded_string)
+        for tree_byte in encoded_tree_bytes:
+            wf.write(tree_byte)
         for data_byte in data_bytes:
             wf.write(data_byte)
         wf.close()
         f.close()
 
-        
+    # use a post order traversal to convert the tree to a list
     def encode_header_tree(self, root):
         if root is None:
             return
         self.encode_header_tree(root.left_child)
         self.encode_header_tree(root.right_child)
         if root.value is not None:
-            self.tree_string += ("1" + root.value)
+            self.tree.append("1")
+            self.tree.append(root.value)
         else:
-            self.tree_string += "0"
+            self.tree.append("0")
+
+    # convert the tree to a bit encoding to save space
+    def header_tree_to_bytes(self, tree_list):
+        if tree_list == []:
+            print("tree_string is empty")
+            return
+        bit_tree_encoding = ""
+        char_flag = False
+        for character in tree_list:
+            if character == "0":
+                bit_tree_encoding += "0"
+            elif character == "1":
+                bit_tree_encoding += "1"
+                char_flag = True
+            else:
+                # get the bit encoding of write_bytes and add it to the bit string
+                if char_flag:
+                    char_bits = format(character, "08b")
+                    bit_tree_encoding += char_bits
+                    char_flag = False
+                else:
+                    print("tree was not formatted correctly")
+                    return
+        tree_bytes = []
+
+        # convert the bit string to bytes that will be written to the output file
+        while len(bit_tree_encoding) > 8:
+            new_byte = bit_tree_encoding[0:8]
+            tree_bytes.append(int(new_byte, 2).to_bytes(1, "big"))
+            bit_tree_encoding = bit_tree_encoding[8:]
+        if bit_tree_encoding != "":
+            while len(bit_tree_encoding) < 8:
+                bit_tree_encoding += "0"
+            tree_bytes.append(int(bit_tree_encoding, 2).to_bytes(1, "big"))
+        return tree_bytes
+
+
+        
+        
 
 
 def main():
     enc = Encoder()
     histogram = enc.get_frequencies(sys.argv[1])
     tree = enc.generate_tree(histogram)
-    enc.find_char_encoding(tree, "")
+    enc.find_byte_encoding(tree, "")
     enc.encode_header_tree(tree)
     enc.write_compressed_file_data(sys.argv[1])
 
